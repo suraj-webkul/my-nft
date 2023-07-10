d4dbc3f40383f5b80fcbd35ba4d69d551b74beea
suraj kashyap <suraj.kashyap370@webkul.in>
2023-07-10 13:26:47 +0530

    Initial commit.

---
+++ scripts/deploy.js
@@ -0,0 +1,18 @@
+async function main() {
+  const MyNFT = await ethers.getContractFactory("MyNFT");
+
+  // Start deployment, returning a promise that resolves to a contract object
+  const myNFT = await MyNFT.deploy();
+
+  await myNFT.deployed();
+
+
+  console.log(`Your deployment address code is: ${myNFT.address}`);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
